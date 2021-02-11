f059e4c037d540d962bcec97f919f9e40c776719
Amitanshu Abhishek <amitanshu@Amitanshus-MacBook-Pro.local>
2021-02-11 23:35:17 +0530

    first commit

---
+++ resources/js/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ASSET_URL_PREFIX: '/apps/pinchandzoom/',
+    BASE_URL: (window.location.origin) + '/apps/pinchandzoom/',
+    API_URL: (window.location.origin) + '/apps/pinchandzoom/api/',
+    API_AUTH_URL: (window.location.origin) + '/apps/pinchandzoom/auth/',
+    URL_PREFIX: '/apps/pinchandzoom/',
+    APP_NAME: 'Zoom Magnifier'
+}
